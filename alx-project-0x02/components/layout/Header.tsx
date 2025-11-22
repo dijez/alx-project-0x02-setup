deb65a00521152749e0ed278065bfca181081e09
dijez <khadijaahmad2k21@gmail.com>
2025-11-22 06:32:32 +0100

    update

---
+++ alx-project-0x02/components/layout/Header.tsx
@@ -0,0 +1,15 @@
+const Header: React.FC = () => {
+    return(
+        <header>
+            <h1>
+                HEADER PAGE
+            </h1>
+            <nav>
+                 <a><li>search bar</li></a>
+            <a><li>sign up</li></a>
+            <a><li>sign in</li></a>
+            <a><li>type of accommodation</li></a>
+            </nav>
+        </header>
+    )
+}
