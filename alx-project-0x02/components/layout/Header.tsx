0f4565d768c862c56705a8964c4dfc47e70b9d18
dijez <khadijaahmad2k21@gmail.com>
2025-11-23 06:30:57 +0100

    update

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -8,7 +8,8 @@ const Header: React.FC = () => {
             </h1>
             <nav>
                 <Link href="/home">HOME</Link><br></br>
-                <Link href="/about">ABOUT</Link>
+                <Link href="/about">ABOUT</Link><br></br>
+                <Link href="/posts">POSTS</Link>
                 <a><li>sign in</li></a>
                 <a><li>type of accommodation</li></a>
             </nav>
---
+++ alx-project-0x02/pages/posts.tsx
@@ -0,0 +1,11 @@
+const Posts: React.FC = () => {
+    return(
+        <div>
+            <h1>
+                POSTS PAGE 
+            </h1>
+        </div>
+    )
+}
+
+export default Posts
